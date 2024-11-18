82877769fce6f3599f70fe252a641ed5637965b4
Ponsajjan <sajjan@mavencart.com>
2024-11-18 19:21:45 +0530

    configured tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div>Hello World</div>
+    <h1 className="text-3xl font-bold underline text-green-500">
+      Hello world!
+    </h1>
   )
 }
 
